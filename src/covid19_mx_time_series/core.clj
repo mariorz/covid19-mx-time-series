68009238884b79c83c89510577227bcbe11ce2aa
Mario Romero <mario@romero.fm>
2020-04-11 19:12:06 -0500

    update for 11-04-2020

--- src/covid19_mx_time_series/core.clj
+++ src/covid19_mx_time_series/core.clj
@@ -3,7 +3,10 @@
             [covid19-mx-time-series.carranco :as carranco]))
 
 
-
+;; currently missing data:
+;; 1) suspect cases from 29-02-2020 to 13-03-2020
+;; as per https://github.com/carranco-sga/Mexico-COVID-19/issues/1
+;; 2) negative cases before 02-04-2020
 
 (defn -main
   [& args])
